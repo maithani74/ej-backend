2cc34988029848d3147645f2eecce80335761e29
maithani74 <maithanir74@gmail.com>
2024-05-28 09:21:18 +0530

    final

--- routes/frontProductRoute.js
+++ routes/frontProductRoute.js
@@ -11,7 +11,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key:process.env.API_KEY ,
     api_secret: process.env.CLOUDINARY_API_SECRET, 
-});
+}); 
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const dotenv = require("dotenv");
 const {
   createProduct,
   getAllProducts,
@@ -15,14 +18,24 @@ const { Product } = require("../model/productModel");
 const { SingleProduct } = require("../model/frontProduct");
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+
+dotenv.config()
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY ,
+    api_secret: process.env.CLOUDINARY_API_SECRET, 
+}); 
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+      folder: 'uploads',
+      format: async (req, file) => 'png', // or any other supported format
+      public_id: (req, file) => Date.now() + '-' + file.originalname,
   },
 });
+
 const upload = multer({ storage: storage });
 
 router
